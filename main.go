596d39891705c709eee2258b9ff071d96a71cfa3
mehmetolgundev <mehmetolgun42konya@gmail.com>
2022-09-25 01:06:01 +0300

    2 added

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func main() {
-	var page = "<html><body><h1>HELLO WORLD</h1></body></html>"
+	var page = "<html><body><h1>HELLO WORLD2</h1></body></html>"
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write([]byte(page))
 	})
